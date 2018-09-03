fbf334e2a5d95c90a515f93738362a106c238215
Gabriel Ochsenhofer <gabs@gabs.tv>
2018-09-03 20:04:07 -0300

    autoitworker debugger

--- au3worker/debug.bat
+++ au3worker/debug.bat
@@ -1,4 +1,4 @@
 ECHO Au3Worker debug mode.
 set /p connStr="Digite o URL do servidor (Go): "
-AutoIt3.exe au3worker.au3 %connStr%
-set /p temp="Aperte qualquer tecla para encerrar"
+"C:\Program Files\AutoIt3\AutoIt3.exe" au3worker.au3 %connStr%
+set /p temp="Aperte ENTER para encerrar"
